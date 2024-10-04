a88a22008e51fe527acc371f1f887649ed685278
Harsh Kashyap <meharshhk@gmail.com>
2024-10-04 11:37:42 +0530

    updated endpoints

--- src/main/java/com/prop/landing/controller/UserController.java
+++ src/main/java/com/prop/landing/controller/UserController.java
@@ -2,7 +2,6 @@ package com.prop.landing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +10,13 @@ import com.prop.landing.entity.User;
 import com.prop.landing.service.UserService;
 import com.prop.landing.util.ResponseStructure;
 
-@CrossOrigin(value = "https://sattvalumina.assetsglobal.in/", allowCredentials = "true")
 @RestController
 public class UserController {
 
 	@Autowired
 	private UserService userService;
 
-	@PostMapping(value = "users/adduser")
+	@PostMapping(value = "users/addLeads")
 	public ResponseEntity<ResponseStructure<User>> addUser(@RequestBody User user) {
 
 		return userService.addUser(user);
